6d321cd50e7ff5a0e327e59c11f298dd4d92725a
Illyoung Choi <iychoi@vm64-219.iplantcollaborative.org>
2015-11-24 15:40:10 -0700

    Add python scripts

---
+++ tools/parse_result.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+import os
+import os.path
+import sys
+import json
+
+total_reads=0
+total_vague_reads=0
+total_classified_reads=0
+total_unknown_reads=0
+
+def _parse(path):
+    print "parsing", path
+    count=0
+    f = open(path)
+    j = json.load(f)
+
+    t = j['total']
+    print "total=" + str(t)
+    global total_reads
+    total_reads += t
+
+    v = j['vague']
+    print "vague=" + str(v)
+    global total_vague_reads
+    total_vague_reads += v
+
+    c = 0
+    if 'classified' in j:
+        c = j['classified']
+    elif 'classifed' in j:
+        c = j['classifed']
+
+    print "classified=" + str(c)
+    global total_classified_reads 
+    total_classified_reads += c
+    
+    u = j['unknown']
+    print "unknown=" + str(u)
+    global total_unknown_reads
+    total_unknown_reads += u
+
+    f.close()
+
+def parse(path):
+    for p in path:
+        _parse(p);
+
+    print "============================"
+    print "grand total=", total_reads
+    print "grand total - vague=", total_vague_reads
+    print "grand total - classified=", total_classified_reads
+    print "grand total - unknown=", total_unknown_reads
+
+
+def main():
+    parse(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
