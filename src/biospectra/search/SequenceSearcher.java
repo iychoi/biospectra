24843ca7650570d7ef3d1fb3db915bc676532c1e
Illyoung Choi <iychoi@email.arizona.edu>
2015-11-26 05:25:20 -0700

    Add proximity query

--- src/biospectra/search/SequenceSearcher.java
+++ src/biospectra/search/SequenceSearcher.java
@@ -36,18 +36,27 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CachingTokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.analysis.tokenattributes.TermToBytesRefAttribute;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.MMapDirectory;
+import org.apache.lucene.util.BytesRef;
 import org.yeastrc.fasta.FASTAEntry;
 import org.yeastrc.fasta.FASTAReader;
 
@@ -160,13 +169,166 @@ public class SequenceSearcher implements Closeable {
         return search(sequence, this.minShouldMatch);
     }
     
+    protected final BooleanQuery createKgramFieldQuery(Analyzer analyzer, BooleanClause.Occur operator, String field, String queryText) {
+        try (TokenStream source = analyzer.tokenStream(field, queryText);
+            CachingTokenFilter stream = new CachingTokenFilter(source)) {
+
+            TermToBytesRefAttribute termAtt = stream.getAttribute(TermToBytesRefAttribute.class);
+            
+            if (termAtt == null) {
+                return null;
+            }
+
+            // phase 1: read through the stream and assess the situation:
+            // counting the number of tokens/positions and marking if we have any synonyms.
+            int numTokens = 0;
+
+            stream.reset();
+            while (stream.incrementToken()) {
+                numTokens++;
+            }
+
+            // phase 2: based on token count, presence of synonyms, and options
+            // formulate a single term, boolean, or phrase.
+            if (numTokens == 0) {
+                return null;
+            } else if (numTokens == 1) {
+                // single term
+                BooleanQuery.Builder q = new BooleanQuery.Builder();
+                q.setDisableCoord(true);
+                
+                TermToBytesRefAttribute termAttB = stream.getAttribute(TermToBytesRefAttribute.class);
+    
+                stream.reset();
+                if (!stream.incrementToken()) {
+                  throw new AssertionError();
+                }
+                
+                Query currentQuery = new TermQuery(new Term(field, BytesRef.deepCopyOf(termAttB.getBytesRef())));
+                q.add(currentQuery, BooleanClause.Occur.MUST);
+                
+                return q.build();
+            } else {
+                BooleanQuery.Builder q = new BooleanQuery.Builder();
+                q.setDisableCoord(false);
+
+                TermToBytesRefAttribute termAttB = stream.getAttribute(TermToBytesRefAttribute.class);
+
+                stream.reset();
+                while (stream.incrementToken()) {
+                    Query currentQuery = new TermQuery(new Term(field, BytesRef.deepCopyOf(termAttB.getBytesRef())));
+                    q.add(currentQuery, BooleanClause.Occur.SHOULD);
+                }
+
+                return q.build();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error analyzing query text", e);
+        }
+    }
+    
+    protected final BooleanQuery create2KgramFieldQuery(Analyzer analyzer, BooleanClause.Occur operator, String field, String queryText) {
+        try (TokenStream source = analyzer.tokenStream(field, queryText);
+            CachingTokenFilter stream = new CachingTokenFilter(source)) {
+
+            TermToBytesRefAttribute termAtt = stream.getAttribute(TermToBytesRefAttribute.class);
+            
+            if (termAtt == null) {
+                return null;
+            }
+
+            // phase 1: read through the stream and assess the situation:
+            // counting the number of tokens/positions and marking if we have any synonyms.
+            int numTokens = 0;
+
+            stream.reset();
+            while (stream.incrementToken()) {
+                numTokens++;
+            }
+
+            // phase 2: based on token count, presence of synonyms, and options
+            // formulate a single term, boolean, or phrase.
+            if (numTokens == 0) {
+                return null;
+            } else if (numTokens == 1) {
+                // single term
+                return null;
+            } else {
+                Term termArr[] = new Term[2];
+                for(int i=0;i<2;i++) {
+                    termArr[i] = null;
+                }
+
+                BooleanQuery.Builder q = new BooleanQuery.Builder();
+                q.setDisableCoord(false);
+                
+                TermToBytesRefAttribute termAttB = stream.getAttribute(TermToBytesRefAttribute.class);
+
+                stream.reset();
+                int count = 0;
+                while (stream.incrementToken()) {
+                    Term t = new Term(field, BytesRef.deepCopyOf(termAttB.getBytesRef()));
+                    if(count % 2 == 0) {
+                        termArr[0] = t;
+                    } else {
+                        termArr[1] = t;
+                        
+                        PhraseQuery.Builder pq = new PhraseQuery.Builder();
+                        pq.setSlop(3);
+                        pq.add(termArr[0]);
+                        pq.add(termArr[1]);
+                        
+                        q.add(pq.build(), BooleanClause.Occur.SHOULD);
+                        
+                        termArr[0] = null;
+                        termArr[1] = null;
+                    }
+                    
+                    count++;
+                }
+
+                return q.build();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error analyzing query text", e);
+        }
+    }
+    
+    private BooleanQuery createQuery(Analyzer analyzer, String field, String queryText, double minShouldMatch) {
+        BooleanQuery kgramQuery = createKgramFieldQuery(analyzer, BooleanClause.Occur.SHOULD, field, queryText);
+        BooleanQuery.Builder builder1 = new BooleanQuery.Builder();
+        builder1.setDisableCoord(kgramQuery.isCoordDisabled());
+        builder1.setMinimumNumberShouldMatch((int) (minShouldMatch * kgramQuery.clauses().size()));
+        for (BooleanClause clause : kgramQuery) {
+            builder1.add(clause);
+        }
+        kgramQuery = builder1.build();
+        
+        BooleanQuery proximityQuery = create2KgramFieldQuery(analyzer, BooleanClause.Occur.SHOULD, field, queryText);
+        BooleanQuery.Builder builder2 = new BooleanQuery.Builder();
+        builder2.setDisableCoord(proximityQuery.isCoordDisabled());
+        builder2.setMinimumNumberShouldMatch((int) (minShouldMatch * proximityQuery.clauses().size()));
+        for (BooleanClause clause : proximityQuery) {
+            builder2.add(clause);
+        }
+        proximityQuery = builder2.build();
+        
+        
+        BooleanQuery.Builder builderFinal = new BooleanQuery.Builder();
+        builderFinal.add(kgramQuery, BooleanClause.Occur.MUST);
+        builderFinal.add(proximityQuery, BooleanClause.Occur.MUST);
+        
+        return builderFinal.build();
+    }
+    
     public List<SearchResult> search(String sequence, double minShouldMatch) throws Exception {
         if(sequence == null || sequence.isEmpty()) {
             throw new IllegalArgumentException("sequence is null or empty");
         }
         
-        QueryParser queryParser = new QueryParser(IndexConstants.FIELD_SEQUENCE, this.quickAnalyzer);
-        Query q = queryParser.createMinShouldMatchQuery(IndexConstants.FIELD_SEQUENCE, sequence, (float) minShouldMatch);
+        //QueryParser queryParser = new QueryParser(IndexConstants.FIELD_SEQUENCE, this.quickAnalyzer);
+        //Query q = queryParser.createMinShouldMatchQuery(IndexConstants.FIELD_SEQUENCE, sequence, (float) minShouldMatch);
+        BooleanQuery q = createQuery(this.quickAnalyzer, IndexConstants.FIELD_SEQUENCE, sequence, minShouldMatch);
         
         int hitsPerPage = 10;
         TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
@@ -177,9 +339,10 @@ public class SequenceSearcher implements Closeable {
         if(hits.length == 0 || hits.length == hitsPerPage) {
             // second trial
             if(this.detailedAnalyzer != null) {
-                queryParser = new QueryParser(IndexConstants.FIELD_SEQUENCE, this.detailedAnalyzer);
-                q = queryParser.createMinShouldMatchQuery(IndexConstants.FIELD_SEQUENCE, sequence, (float) minShouldMatch);
-
+                //queryParser = new QueryParser(IndexConstants.FIELD_SEQUENCE, this.detailedAnalyzer);
+                //q = queryParser.createMinShouldMatchQuery(IndexConstants.FIELD_SEQUENCE, sequence, (float) minShouldMatch);
+                q = createQuery(this.detailedAnalyzer, IndexConstants.FIELD_SEQUENCE, sequence, minShouldMatch);
+                
                 collector = TopScoreDocCollector.create(hitsPerPage);
                 this.indexSearcher.search(q, collector);
                 topdocs = collector.topDocs();
@@ -252,8 +415,9 @@ public class SequenceSearcher implements Closeable {
                 @Override
                 public void run() {
                     try {
-                        QueryParser queryParser = new QueryParser(IndexConstants.FIELD_SEQUENCE, quickAnalyzer);
-                        Query q = queryParser.createMinShouldMatchQuery(IndexConstants.FIELD_SEQUENCE, sequence, (float) _minShouldMatch);
+                        //QueryParser queryParser = new QueryParser(IndexConstants.FIELD_SEQUENCE, quickAnalyzer);
+                        //BooleanQuery q = queryParser.createMinShouldMatchQuery(IndexConstants.FIELD_SEQUENCE, sequence, (float) _minShouldMatch);
+                        BooleanQuery q = createQuery(quickAnalyzer, IndexConstants.FIELD_SEQUENCE, sequence, _minShouldMatch);
                         
                         int hitsPerPage = 10;
                         TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
@@ -283,8 +447,9 @@ public class SequenceSearcher implements Closeable {
                         if(bresult.getType() != SearchResultType.CLASSIFIED || hits.length == hitsPerPage) {
                             // detailed - second trial
                             if(detailedAnalyzer != null) {
-                                queryParser = new QueryParser(IndexConstants.FIELD_SEQUENCE, detailedAnalyzer);
-                                q = queryParser.createMinShouldMatchQuery(IndexConstants.FIELD_SEQUENCE, sequence, (float) _minShouldMatch);
+                                //queryParser = new QueryParser(IndexConstants.FIELD_SEQUENCE, detailedAnalyzer);
+                                //q = queryParser.createMinShouldMatchQuery(IndexConstants.FIELD_SEQUENCE, sequence, (float) _minShouldMatch);
+                                q = createQuery(detailedAnalyzer, IndexConstants.FIELD_SEQUENCE, sequence, _minShouldMatch);
 
                                 collector = TopScoreDocCollector.create(hitsPerPage);
                                 indexSearcher.search(q, collector);
